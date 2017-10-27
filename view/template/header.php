94d6b933a86ecef7c4c7558b8dd52281ecc4e86c
remilefebvre <lefebvre.reminicolas@gmail.com>
2017-10-27 10:22:12 +0200

    add header phone v1

--- view/template/header.php
+++ view/template/header.php
@@ -9,9 +9,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Font family -->
+  <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
 
   <!-- Fontawersome -->
-  <!-- <script src="https://use.fontawesome.com/4d141429f4.js"></script> -->
+  <script src="https://use.fontawesome.com/4d141429f4.js"></script>
+
 
   <link rel="icon" href="img/logo.png">
   <link rel="apple-touch-icon" href="apple-touch-icon.png">
@@ -30,76 +32,44 @@
 <body>
 
 <header class="fixed-top">
+  <h1>Car <br><span>Dealer</span></h1>
+  <div class="one"></div>
+  <div class="two"></div>
+  <div class="three"></div>
+  <div class="foor"></div>
+  <div class="five"></div>
+  <div class="six"></div>
+  <div class="seven"></div>
+  <div class="height"></div>
+  <div class="nine"></div>
 
-  <!-- NAV BAR -->
-  <nav class="navbar navbar-toggleable-md navbar-light">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <a href="index.php">
-      <h1 class="navbar-brand" >
-        <figure id="logo">
-          <img src="img/tireTracks.png" alt="logo">
-        </figure>
-      </h1>
-    </a>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="pr-5 nav-item active">
-          <a class="nav-link" href="index.php">Linsting vehicule</a>
-        </li>
-        <?php if (!isset($_POST["detailVehicule"])) {
-          ?>
-          <li class="pr-5 nav-item ">
-            <form class="d-flex flex-row" action="index.php" method="post">
-              <select name="filtre" class="nav-link custom-select">
-                <option class="pr-4" selected>Filtres</option>
-                <option value="name">Name</option>
-                <option value="model">Model</option>
-                <option value="type">Type</option>
-              </select>
-              <input type="submit" value="OK">
-            </form>
-          </li>
-          <?php
-        } ?>
-        <li class="pr-5 nav-item active ">
-          <a class="nav-link addVehicule">Add vehicule</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-
-
-
-
-  <!-- ADD VEHICULE BAR -->
-  <nav class="navbar bg-faded" id="addVehiculeBar">
-    <h1 class="navbar-brand" href="#">Add vehicule</h1>
-    <div class="">
-      <ul class="navbar-nav">
-        <form class="" action="index.php" method="post">
-          <li class="pr-5 nav-item active">
-            <input type="text" name="name" placeholder="Name" required>
-          </li>
-          <li class="pr-5 nav-item active">
-            <input type="text" name="model" placeholder="Modele" required>
-          </li>
-          <li class="pr-5 nav-item active">
-            <input type="text" name="detail" placeholder="Detail" required>
-          </li>
-          <li class="pr-5 nav-item d-flex flex-row ">
-            <select name="type" class="nav-link custom-select" required>
-              <option class="pr-4" selected>Type</option>
-              <option value="truck">Truck</option>
-              <option value="car">Car</option>
-              <option value="moto">Moto</option>
-            </select>
-            <input type="submit" name="add" value="Add">
-          </li>
-        </form>
-      </ul>
-    </div>
-  </nav>
 
 </header>
+<!-- ADD VEHICULE BAR -->
+<nav class="navbar bg-faded" id="addVehiculeBar">
+  <h1 class="navbar-brand" href="#">Add vehicule</h1>
+  <div class="">
+    <ul class="navbar-nav">
+      <form class="" action="index.php" method="post">
+        <li class="pr-5 nav-item active">
+          <input type="text" name="name" placeholder="Name" required>
+        </li>
+        <li class="pr-5 nav-item active">
+          <input type="text" name="model" placeholder="Modele" required>
+        </li>
+        <li class="pr-5 nav-item active">
+          <input type="text" name="detail" placeholder="Detail" required>
+        </li>
+        <li class="pr-5 nav-item d-flex flex-row ">
+          <select name="type" class="nav-link custom-select" required>
+            <option class="pr-4" selected>Type</option>
+            <option value="truck">Truck</option>
+            <option value="car">Car</option>
+            <option value="moto">Moto</option>
+          </select>
+          <input type="submit" name="add" value="Add">
+        </li>
+      </form>
+    </ul>
+  </div>
+</nav>
