876ccc989de3ccc0eac4abd288dd32c940bbdb99
remilefebvre <lefebvre.reminicolas@gmail.com>
2017-10-27 15:51:35 +0200

    add header done

--- view/template/header.php
+++ view/template/header.php
@@ -9,8 +9,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Font family -->
-  <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Contrail+One" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:700" rel="stylesheet">
 
 
   <!-- Fontawersome -->
@@ -33,50 +33,92 @@
 
 <body>
 
-<header class="fixed-top">
-  <h1>Car <br><span>Dealer</span></h1>
-  <div class="one"></div>
-  <div class="two">
-    <nav class="menu">
-      <ul>
-        <li><a href="index.php">Listing</a></li>
-        <li><a class="addVehicule" href="#">Add Vehicule</a></li>
-      </ul>
+  <header class="fixed-top">
+    <nav class="navHeader">
+      <h1>Car <br><span>Dealer</span></h1>
+      <div class="one"></div>
+      <div class="two">
+        <nav class="menu">
+          <ul>
+            <li><a tabindex="-1" href="index.php">Listing</a></li>
+            <li><a tabindex="-1" class="addVehicule" href="#">Add Vehicle</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div class="three"></div>
+      <div class="foor"></div>
+      <div class="five"></div>
+      <div class="six"></div>
+      <div class="seven"></div>
+      <div class="height"></div>
+      <div class="nine"></div>
+
     </nav>
-  </div>
-  <div class="three"></div>
-  <div class="foor"></div>
-  <div class="five"></div>
-  <div class="six"></div>
-  <div class="seven"></div>
-  <div class="height"></div>
-  <div class="nine"></div>
-</header>
-<!-- ADD VEHICULE BAR -->
-<nav class="navbar bg-faded" id="addVehiculeBar">
-  <h1 class="navbar-brand" href="#">Add vehicule</h1>
-  <div class="">
-    <ul class="navbar-nav">
-      <form class="" action="index.php" method="post">
-        <li class="pr-5 nav-item active">
-          <input type="text" name="name" placeholder="Name" required>
-        </li>
-        <li class="pr-5 nav-item active">
-          <input type="text" name="model" placeholder="Modele" required>
-        </li>
-        <li class="pr-5 nav-item active">
-          <input type="text" name="detail" placeholder="Detail" required>
-        </li>
-        <li class="pr-5 nav-item d-flex flex-row ">
-          <select name="type" class="nav-link custom-select" required>
-            <option class="pr-4" selected>Type</option>
-            <option value="truck">Truck</option>
-            <option value="car">Car</option>
-            <option value="moto">Moto</option>
-          </select>
-          <input type="submit" name="add" value="Add">
-        </li>
-      </form>
-    </ul>
-  </div>
-</nav>
+    <!-- ADD VEHICULE BAR -->
+    <div class="navbar" id="addVehiculeBar">
+      <div class="banner">
+        <h2 class="" href="#">Add vehicle</h2>
+        <div class="container">
+          <div class="row ">
+            <div class="col-md-12">
+              <form action="index.php" method="post" enctype="multipart/form-data">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <input name="Name" type="text" class="form-control" placeholder="Name" required="required" />
+                    </div>
+                    <div class="form-group">
+                      <input name="model" type="text" class="form-control" placeholder="Model" required="required" />
+                    </div>
+                    <select name="type" class="nav-link custom-select" required>
+                        <option class="pr-4" selected>Type</option>
+                        <option value="truck">Truck</option>
+                        <option value="car">Car</option>
+                        <option value="moto">Moto</option>
+                      </select>
+                    <p class="mb-0 mt-5 w-100"><strong>Add picture</strong></p>
+                    <input class="mb-3 " name="image" type="file" value="Parcourir..">
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <textarea name="detail" class="form-control" rows="9" cols="25" required="required" placeholder="Detail"></textarea>
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                    <button type="submit" class="btnAdd btn pull-right">
+                        Add
+                      </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        </nav>
+  </header>
+  <!--
+<div class="">
+ <ul class="navbar-nav">
+ <form class="" action="index.php" method="post">
+ <li class="pr-5 nav-item active">
+ <input type="text" name="name" placeholder="Name" required>
+</li>
+<li class="pr-5 nav-item active">
+<input type="text" name="model" placeholder="Modele" required>
+</li>
+<li class="pr-5 nav-item active">
+<input type="text" name="detail" placeholder="Detail" required>
+</li>
+<li class="pr-5 nav-item d-flex flex-row ">
+<select name="type" class="nav-link custom-select" required>
+<option class="pr-4" selected>Type</option>
+<option value="truck">Truck</option>
+<option value="car">Car</option>
+<option value="moto">Moto</option>
+</select>
+<input type="submit" name="add" value="Add">
+</li>
+</form>
+</ul>
+</div> -->
